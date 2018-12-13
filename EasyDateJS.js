68a290d52efbbbbe754ca1f143bc7b4fec9ca544
GBortoto <bortoto23@gmail.com>
2018-12-13 14:01:50 -0200

    Comments! :) + removed SetValues + README updated

--- EasyDateJS.js
+++ EasyDateJS.js
@@ -1,5 +1,6 @@
 class EasyDate
 {
+    // IMPORTANT: This class handles the epoch in SECONDS, not MILLISECONDS
     constructor(day, month, year)
     {
         this.day = day;
@@ -7,33 +8,38 @@ class EasyDate
         this.month = month;
     }
 
-    setDate(day, month, year)
-    {
-        this.day = day;
-        this.year = year;
-        this.month = month;
-    }
-
+    // Returns a native Date object based on the values stored in the instance
     getDateObject()
     {
-        return new Date(this.year, this.month-1, this.day);
+        // IMPORTANT: JS counts months from 0 to 11, so december is represented as 0
+        //              For this reason, the local variable month has to be processed
+        var month = this.month == 12 ? 0 : this.month;
+        return new Date(this.year, month, this.day);
     }
 
+    // Returns how many seconds there were between the first of january of 1970 and the current date
     getEpoch()
     {
         return Date.parse(this.getDateObject()) / 1000;
     }
 
+    // Returns a string in the format "dd/mm/yyyy"
     toString()
     {
         return this.day + "/" + this.month + "/" + this.year;
     }
 
-    static convertDaysToEpoch(days)
+    // Static method - Returns the length of N days in seconds
+    static convertDaysToEpoch(numberOfDays)
     {
-        return days * 24 * 60 * 60;
+        return numberOfDays * 24 * 60 * 60;
     }
 
+    // Static method - Given two dates, compare the two and tell which one comes first
+    // Possible outputs:
+    //  1   --> firstDate > secondDate
+    //  0   --> firstDate = secondDate
+    //  -1  --> firstDate < secondDate
     static compareDates(firstDate, secondDate)
     {
         var firstDateEpoch = firstDate.getEpoch();
